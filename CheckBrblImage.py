696abe879b6d7b4ba02d1fd132eee19f7e69434d
helscn <helscns@gmail.com>
2025-12-24 21:03:09 +0800

    增加表格数据显示

--- CheckBrblImage.py
+++ CheckBrblImage.py
@@ -4,12 +4,16 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog, QHeaderView, QStyleFactory
 from PySide6.QtCore import Qt, QAbstractTableModel, QModelIndex
 from PySide6.QtCore import QThread, Signal, Slot, QObject
+from PySide6.QtGui import QPixmap
 from ui.MainWindow_ui import Ui_MainWindow
 import resources_rc
 
 from library import MonitorDir
 import json
 import os
+import re
+
+
 
 class ImagesTableModel(QAbstractTableModel):
     def __init__(self, data, headers):
@@ -48,18 +52,27 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.load_config()
-        self.dirs=[]
-        self.images={}
+        self.dirs = []
+        self.images = {}
+        self.data = []
+        self.currentIndex = -1
+        self.status = {
+            "ok": "✅",
+            "section": "⭕",
+            "error":"⚠️",
+            "delete": "❌"
+        }
         
-        self.init_ui()
+        self.setWindowTitle("波若波罗板厚测量确认")
+        self.init_table()
         self.init_thread()
         self.init_signal()
 
 
     def load_config(self):
         config={
-            "ImagesFolder":"D:/Images/",
-            "ThumbnailFolder":"D:/Thumbnails/"
+            "ImagesFolder":"E:/Images/",
+            "ThumbnailFolder":"E:/Thumbnails/"
         }
         if os.path.isfile("config.json"):
             try:
@@ -69,15 +82,9 @@ class MainWindow(QMainWindow):
                 QMessageBox.critical(self, "配置文件错误", f"无法读取配置文件:\n{e}")
         self.config = config
 
-    def init_ui(self):
-        # 设置表格表头
-        self.setWindowTitle("波若波罗板厚测量确认")
+    def init_table(self):
         self.TableHeaders=['二维码',' 板厚图片 ',' 确认结果 ']
-        data=[]
-        for i in range(36):
-            data.append(["3075513802","✅","⭕"])
-            data.append(["3075513802","✅","❌"])
-        tableModel=ImagesTableModel(data,self.TableHeaders)
+        tableModel=ImagesTableModel([],self.TableHeaders)
         self.ui.tblImages.setModel(tableModel)
         header = self.ui.tblImages.horizontalHeader()
         header.setSectionResizeMode(0,QHeaderView.ResizeMode.Stretch)
@@ -89,37 +96,27 @@ class MainWindow(QMainWindow):
         self.worker = MonitorDir(self.config["ImagesFolder"], self.config["ThumbnailFolder"])
         self.worker.moveToThread(self.thread)
         self.worker.dir_updated.connect(self.dir_updated)
+        self.worker.thumbnail_updated.connect(self.thumbnail_updated)
+        self.worker.error_occurred.connect(self.error_occurred)
         self.thread.started.connect(self.worker.start_monitor)
         self.thread.start()
         
     def init_signal(self):
-        self.ui.btnLoadImages.clicked.connect(self.load_images)
+        self.ui.cmbSelectPN.currentIndexChanged.connect(self.update_table)
         self.ui.btnMoveTodo.clicked.connect(self.move_todo)
-        self.ui.btnPrevious.clicked.connect(self.previous_image)
-        self.ui.btnNext.clicked.connect(self.next_image)
         self.ui.btnMarkOK.clicked.connect(self.mark_ok)
         self.ui.btnMarkSection.clicked.connect(self.mark_section)
         self.ui.btnMarkDelete.clicked.connect(self.mark_delete)
+        self.ui.tblImages.clicked.connect(self.select_table_row)
 
     def closeEvent(self, event):
-        print("Closing application...")
         self.worker.stop()
         self.thread.quit()
         if not self.thread.wait(3000):  # 等待3秒
-            print("Thread didn't finish gracefully, terminating...")
             self.thread.terminate()  # 最后手段
             self.thread.wait()
         event.accept()
-    def load_images(self):
-        pass
-
     def move_todo(self):
-        pass
-
-    def previous_image(self):
-        pass
-
-    def next_image(self):
         pass
 
     def mark_ok(self):
@@ -131,9 +128,37 @@ class MainWindow(QMainWindow):
     def mark_delete(self):
         pass
 
+    
+    def update_table(self):
+        folder_regex = re.compile(r"^([A-Z0-9\-]+)_(\d+)_(\d{14})_(\d*)_.*$",re.IGNORECASE)
+        current_dir = self.ui.cmbSelectPN.currentText()
+        self.data = [(dir,name) for dir,name in self.images.keys() if dir == current_dir]
+        self.data.sort(key=lambda x: x[1])
+        tableData=[]
+        for dir,name in self.data:
+            match = folder_regex.match(name)
+            if match:
+                matrix = match.group(4)
+            else:
+                matrix = ""
+            image = self.images[(dir,name)]
+            tableData.append([matrix,image["status"],image['checked']])
+            
+        tableModel=ImagesTableModel(tableData,self.TableHeaders)
+        self.ui.tblImages.setModel(tableModel)
+
+    def select_table_row(self):
+        self.currentIndex = self.ui.tblImages.currentIndex().row()
+        self.show_selected_image(self.currentIndex)
+
+
+    def show_selected_image(self,idx:int=-1):
+        if idx>=0:
+            self.currentIndex = idx
+        
+
     @Slot(list)
     def dir_updated(self, dirs: list):
-        print("接收目录更新")
         current_dir = self.ui.cmbSelectPN.currentText()
         self.ui.cmbSelectPN.clear()
         for dir in dirs:
@@ -143,8 +168,29 @@ class MainWindow(QMainWindow):
 
 
     @Slot(str, str)
-    def thumbnail_updated(self, dir:str, name:str):
-        pass
+    def thumbnail_updated(self, dir:str, name:str, status:str):
+        if status == "ok":
+            if (dir,name) in self.images:
+                self.images[(dir,name)]["status"] = self.status[status]
+            else:
+                self.images[(dir,name)] = {"status": self.status[status], "checked": ""}
+        elif status == "error":
+            if (dir,name) in self.images:
+                self.images[(dir,name)]["status"] = self.status[status]
+            else:
+                self.images[(dir,name)] = {"status": self.status[status], "checked": ""}
+        elif status == "delete":
+            if (dir,name) in self.images:
+                del self.images[(dir,name)]
+        elif status == "new":
+            self.images[(dir,name)] = {"status": "", "checked": ""}
+        if self.ui.cmbSelectPN.currentText() == dir:
+            self.update_table()
+
+
+    @Slot(str)
+    def error_occurred(self, error:str):
+        QMessageBox.critical(self, "错误", error)
 
 
 
